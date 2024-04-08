e3beaa38c895be3772caf1351d848c1607785f72
llursmanashvili <lukacho.lursmanashvili+t2@gmail.com>
2024-04-08 18:31:12 +0400

    new changes

--- src/main/main.js
+++ src/main/main.js
@@ -20,6 +20,7 @@ import women from "./assets/women.jpg";
 import gsa from "./assets/gsa.jpg";
 import gsp from "./assets/gsp.png";
 import tepe from "./assets/tepe.png";
+import ciu from "./assets/ciu.jpg";
 
 function ParallaxText({ children, baseVelocity = 100 }) {
   const baseX = useMotionValue(0);
@@ -90,6 +91,7 @@ export function Paralax() {
         <img style={{ width: "40vh", height: "20vh" }} src={women} />
         <img style={{ width: "40vh", height: "20vh" }} src={gsa} />
         <img style={{ width: "40vh", height: "20vh" }} src={gsp} />
+        <img style={{ width: "28vh", height: "20vh" }} src={ciu} />
         <img style={{ width: "40vh", height: "20vh" }} src={tepe} />
         <img style={{ width: "40vh", height: "20vh" }} src={ads} />
       </ParallaxText>
@@ -109,6 +111,7 @@ export function Paralaxen() {
         <img style={{ width: "40vh", height: "20vh" }} src={women} />
         <img style={{ width: "40vh", height: "20vh" }} src={gsa} />
         <img style={{ width: "40vh", height: "20vh" }} src={gsp} />
+        <img style={{ width: "28vh", height: "20vh" }} src={ciu} />
         <img style={{ width: "40vh", height: "20vh" }} src={tepe} />
         <img style={{ width: "40vh", height: "20vh" }} src={adsen} />
       </ParallaxText>
